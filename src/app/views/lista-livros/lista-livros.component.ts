5c3b76160fef16a13b0cb0c983d62355d6454704
Lucas Grolli Arruz <lucas@cittainformatica.com.br>
2024-04-29 16:39:52 -0300

    adding filters and refactorating the code. Final project

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,10 +1,23 @@
 import { FormControl } from '@angular/forms';
-import { VolumeInfo, ImageLinks, Item } from './../../models/interfaces';
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription, map, switchMap, tap } from 'rxjs';
-import { Livro } from 'src/app/models/interfaces';
+import { Item, LivrosResultado } from './../../models/interfaces';
+import { Component } from '@angular/core';
+import {
+  catchError,
+  debounce,
+  debounceTime,
+  distinctUntilChanged,
+  empty,
+  filter,
+  map,
+  of,
+  switchMap,
+  tap,
+  throwError,
+} from 'rxjs';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 300;
 
 @Component({
   selector: 'app-lista-livros',
@@ -13,12 +26,30 @@ import { LivroService } from 'src/app/service/livro.service';
 })
 export class ListaLivrosComponent {
   campoBusca = new FormControl();
+  mensagemErro = '';
+  livrosResultado: LivrosResultado;
 
   constructor(private service: LivroService) {}
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => {
+      console.log('Fluxo inicial de dados');
+    }),
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-    map((items) => this.livrosResultadoParaLivros(items))
+    map((resultado) => (this.livrosResultado = resultado)),
+    map((resultado) => resultado.items ?? []),
+    map((items) => this.livrosResultadoParaLivros(items)),
+    catchError((erro) => {
+      console.log(erro);
+      return throwError(
+        () =>
+          new Error(
+            (this.mensagemErro = `Ops, ocorreu um erro! Recarregue a aplicação!`)
+          )
+      );
+    })
   );
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
